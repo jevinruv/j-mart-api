e9949f96f689f99149281dc2b1495d8a2a95976e
Jevin Fernando <jevinruv@gmail.com>
2019-04-01 19:00:56 +0530

    added remote db & setup for heroku

--- src/main/java/com/jevin/jmartapi/JMartApiApplication.java
+++ src/main/java/com/jevin/jmartapi/JMartApiApplication.java
@@ -9,16 +9,4 @@ public class JMartApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(JMartApiApplication.class, args);
     }
-
-/*    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }*/
 }
